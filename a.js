d3713b6f480352be4c161489c8bd37d497755e38
任齐双 <renqishuang@gwssi.com.cn>
2019-07-15 13:29:27 +0800

    1

--- a.js
+++ a.js
@@ -1,11 +1,1 @@
-var name = 'rqs';
-function getName(){
-    return name;
-}
-var a = '哈哈, 冲突起来吧';
-var b = {
-    age:26,
-    name:'李四'
-}
-var c = 'HelloWorld';
-var d = 20190528;
+var name = '张三';
